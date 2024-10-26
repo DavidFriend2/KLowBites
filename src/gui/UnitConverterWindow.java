a309d67ab165e19ea8798b5d691ad22cffba15a9
jayden <jayden@DESKTOP-46SHCQ6.hsd1.va.comcast.net>
2024-10-26 09:44:44 -0400

    Added icons to Converter window

--- src/gui/UnitConverterWindow.java
+++ src/gui/UnitConverterWindow.java
@@ -20,15 +20,50 @@ public class UnitConverterWindow extends JFrame {
         JMenuBar menuBar = new JMenuBar();
         setJMenuBar(menuBar);
 
-
         // Create main content panel
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BorderLayout());
+        JPanel mainPanel = new JPanel(new BorderLayout());
         
+        // Create image panel with FlowLayout aligned to the left
+        JPanel imagePanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 10));
+        
+        // Add image buttons to the image panel
+        addImageButton(imagePanel, "/img/calculate.png", "Calculate");
+        addImageButton(imagePanel, "/img/reset.png", "Rest");
+
+        // Add image panel to the top (NORTH) of the main panel
+        mainPanel.add(imagePanel, BorderLayout.NORTH);
+
         // Add a label to the center of the panel
         JLabel label = new JLabel("TODO", SwingConstants.CENTER);
         mainPanel.add(label, BorderLayout.CENTER);
 
         add(mainPanel);
+    }
+
+    private void addImageButton(JPanel panel, String imagePath, String buttonName) {
+        ImageIcon icon = createImageIcon(imagePath);
+        if (icon != null) {
+            Image img = icon.getImage().getScaledInstance(40, 40, Image.SCALE_SMOOTH);
+            JButton button = new JButton(new ImageIcon(img));
+            button.setPreferredSize(new Dimension(50, 50));
+            button.setToolTipText(buttonName);
+            panel.add(button);
+        }
+    }
+
+    private ImageIcon createImageIcon(String path) {
+        java.net.URL imgURL = getClass().getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new UnitConverterWindow().setVisible(true);
+        });
     }
 }
