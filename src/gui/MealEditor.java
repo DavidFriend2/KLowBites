1cb54e416e9ec838474274909212838d04ccf584
nathan kirby <kirbync@dukes.jmu.edu>
2024-10-28 22:12:17 -0400

    finished basic meal editor gui

--- src/gui/MealEditor.java
+++ src/gui/MealEditor.java
@@ -1,12 +1,18 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 /**
@@ -30,8 +36,8 @@ public class MealEditor extends JFrame
 	{
 		setTitle("KiLowBites Meal Editor");
 		setSize(600,400);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    setLocationRelativeTo(null);
+		setResizable(false);
+		setLocationRelativeTo(null);
 	    
 	    // Create main content panel
         JPanel mainPanel = new JPanel(new BorderLayout());
@@ -46,14 +52,54 @@ public class MealEditor extends JFrame
         buttonCreation.addImageButton(imagePanel, "/img/save.png", "Save");
         buttonCreation.addImageButton(imagePanel, "/img/saveAs.png", "Save As");
         buttonCreation.addImageButton(imagePanel, "/img/close.png", "Close");
+        imagePanel.getComponent(0).setEnabled(false);
+        imagePanel.getComponent(1).setEnabled(false);
+        imagePanel.getComponent(2).setEnabled(false);
+        imagePanel.getComponent(3).setEnabled(false);
+        imagePanel.getComponent(4).setEnabled(false);
         
         // Add image panel to the top (NORTH) of the main panel
         mainPanel.add(imagePanel, BorderLayout.NORTH);
 
-        // Add a label to the center of the panel
-        JLabel label = new JLabel("TODO", SwingConstants.CENTER);
-        mainPanel.add(label, BorderLayout.CENTER);
+        // text box panel
+        JPanel textPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JTextField textBox = new JTextField(20);
+        JLabel textLabel = new JLabel("Name:");
+        textPanel.setBorder(BorderFactory.createEmptyBorder(0,6,0,0));
+        textBox.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        textPanel.add(textLabel);
+        textPanel.add(textBox);
+        mainPanel.add(textPanel, BorderLayout.CENTER);
+        
+        // add recipe window
+        JPanel recipeBox = new JPanel(new BorderLayout());
+        recipeBox.setPreferredSize(new Dimension(300,265));
+        recipeBox.setBorder(BorderFactory.createCompoundBorder(
+        		BorderFactory.createEmptyBorder(10, 10, 10, 10), BorderFactory.createLineBorder(Color.BLACK, 2)));
+        
+        //top half of recipe box
+        JPanel recipeBoxTop = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JButton addRecipe = new JButton("Add Recipe");
+        recipeBoxTop.add(addRecipe, BorderLayout.EAST);
+        recipeBox.add(recipeBoxTop, BorderLayout.NORTH);
+        
+        
+        //bottom half of recipe box
+        JPanel recipeBoxBottom = new JPanel(new BorderLayout());
+        JTextArea recipes = new JTextArea(12, 48);
+        JScrollPane scroll = new JScrollPane(recipes);
+        recipeBoxBottom.add(scroll, BorderLayout.WEST);
+        //bottom right half
+        JPanel bottomRight = new JPanel(new BorderLayout());
+        JButton delete = new JButton("Delete");
+        bottomRight.add(delete, BorderLayout.SOUTH);
+        recipeBoxBottom.add(bottomRight, BorderLayout.EAST);
+        recipeBox.add(recipeBoxBottom);
 
+        //add recipe box
+        mainPanel.add(recipeBox,BorderLayout.SOUTH);
+        
+        //add main panel
         add(mainPanel);
 	}
 	
