4e5d2a0457c34cc378ad8249d3c03fe90fb48af6
Ryan Mendez <mendezrh@dukes.jmu.edu>
2024-11-12 10:37:07 -0500

    added a meal to test, and the other recipes

--- src/Information/RecipeFileCreator.java
+++ src/Information/RecipeFileCreator.java
@@ -6,24 +6,39 @@ package Information;
 public class RecipeFileCreator
 {
 
-//  public static void main(String[] args)
-//  {
+  public static void main(String[] args)
+  {
 //    // Retrieve the recipes list from the Recipe class
 //    List<Recipe> recipes = Recipe.getRecipes();
 //
-//    // Save each recipe to a separate file
-//    for (Recipe recipe : recipes)
+//    // Meal testMeal = new Meal("Good Eats", recipes);
+//    Recipe recipe = recipes.get(2);
+//    try
 //    {
-//      try
-//      {
-//        recipe.saveRecipeToFile();
-//        System.out.println("Saved recipe: " + recipe.getName() + " to " + recipe.getFileName());
-//      }
-//      catch (IOException e)
-//      {
-//        System.out.println("Failed to save recipe: " + recipe.getName());
-//        e.printStackTrace();
-//      }
+//      recipe.saveRecipeToFile(recipe.getFileName());
+//      System.out.println("Saved recipe: " + recipe.getName() + " to " + recipe.getFileName());
 //    }
-//  }
+//    catch (IOException e)
+//    {
+//      System.out.println("Failed to save recipe: " + recipe.getName());
+//      e.printStackTrace();
+//    }
+//
+//    // Save each recipe to a separate file
+//    // for (Recipe recipe : recipes)
+//    // {
+//    // try
+//    // {
+//    // recipe.saveRecipeToFile();
+//    // System.out.println("Saved recipe: " + recipe.getName() + " to " + recipe.getFileName());
+//    // }
+//    // catch (IOException e)
+//    // {
+//    // System.out.println("Failed to save recipe: " + recipe.getName());
+//    // e.printStackTrace();
+//    // }
+//    // }
+//    // }
+
+  }
 }
