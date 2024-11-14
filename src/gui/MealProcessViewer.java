1511bf2492e1d67fc016ba05b059b3ab312374b9
josh browning <brownije@dukes.jmu.edu>
2024-11-13 19:29:08 -0500

    meal process and recipe process minus plating time

--- src/gui/MealProcessViewer.java
+++ src/gui/MealProcessViewer.java
@@ -1,14 +1,333 @@
 package gui;
 
-public class MealProcessViewer {
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.print.*;
+import java.util.ArrayList;
+import java.util.List;
 
-	public MealProcessViewer() {
-		// TODO Auto-generated constructor stub
+import javax.swing.*;
+
+import Information.*;
+
+public class MealProcessViewer extends JFrame implements Printable 
+{
+
+	private static final long serialVersionUID = 1L;
+	private static final String PRINT_ICON_PATH = "/img/print.png";
+	private static final String OPEN_ICON_PATH = "/img/open.png";
+	private Meal meal;
+	private Recipe recipe;
+	private boolean doubleBuffered;
+    private JComponent delegate;
+    private JButton printButton, openButton;
+    private JPanel printPanel, mainPanel, utensilsPanel, stepsPanel;
+    private List<Meal> selectedMeals = new ArrayList<>();
+	
+	public MealProcessViewer() 
+	{
+	    initialize();
 	}
+	
+	public MealProcessViewer(Meal meal) 
+	{
+		this.meal = meal;
+        initialize();
+        setTitle("KiLowBites Process Viewer: " + meal.getName());
+	}
+	
+	private void initialize() 
+	{
+        setTitle("KiLowBites Process Viewer");
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLayout(new BorderLayout());
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        mainPanel = new JPanel(new BorderLayout());
+        printPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 10));
+        mainPanel.add(printPanel, BorderLayout.NORTH);
+        
+        // Add utensils 
+        utensilsPanel = new JPanel(new BorderLayout());
+	    JScrollPane utensilsScroll = createUtensilsPanel();
+	    utensilsPanel.add(utensilsScroll, BorderLayout.CENTER);
+	    utensilsPanel.setPreferredSize(new Dimension(200, 100));
 
+        // Add steps
+        stepsPanel = new JPanel(new BorderLayout());
+        JScrollPane stepsScroll = createStepsPanel();
+        stepsPanel.add(stepsScroll, BorderLayout.CENTER);
+	    stepsPanel.setPreferredSize(new Dimension(200, 300));
+	    
+	    // Add both to main panel
+	    mainPanel.add(utensilsPanel, BorderLayout.CENTER);
+        mainPanel.add(stepsPanel, BorderLayout.SOUTH);
+
+        add(mainPanel, BorderLayout.CENTER);
+        addPrintButton();
+        addOpenButton();
+    }
+	
+	private void addPrintButton() 
+	{
+        printButton = createButton(PRINT_ICON_PATH, 50, 50, "Print");
+        printButton.addActionListener(new ActionListener() 
+        {
+            @Override
+            public void actionPerformed(ActionEvent e) 
+            {
+                printRecipe();
+            }
+        });
+        printPanel.add(printButton);
+    }
+	
+	private void addOpenButton() 
+	{
+        openButton = createButton(OPEN_ICON_PATH, 50, 50, "Open");
+        openButton.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e) 
+            {
+                new MealSearcher().setVisible(true);
+            }
+        });
+        printPanel.add(openButton);
+    }
+	
+	private void printRecipe() 
+	{
+        PrinterJob job = PrinterJob.getPrinterJob();
+        job.setPrintable(this);
+
+        if (job.printDialog()) 
+        {
+            try 
+            {
+                job.print();
+            } 
+            catch (PrinterException e) 
+            {
+                JOptionPane.showMessageDialog(this, "Unable to print", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+	
+	private JScrollPane createUtensilsPanel() 
+	{
+		
+		JScrollPane utensilsPanel = new JScrollPane();
+        utensilsPanel.setPreferredSize(new Dimension(10,1000));
+		DefaultListModel<String> DLM = new DefaultListModel<>();
+		JList<String> utensilsList = new JList<>(DLM);
+
+	    utensilsPanel = new JScrollPane(utensilsList);
+	    
+		if (this.meal != null) 
+		{
+			for (Recipe recipe : meal.getRecipes())
+			{
+			
+			    // Add items to utensils list
+			    for (Utensil utensil : recipe.getUtenils())	    
+			    {
+			        String name = utensil.getName();
+			        String details = utensil.getDetails() != null ? utensil.getDetails() : "";
+			        DLM.addElement(name + ": " + details);
+			    }
+			}
+		}
+		
+	    
+	    // Create border with title
+	    utensilsPanel.setBorder(BorderFactory.createTitledBorder("Utensils"));
+	    
+	    return utensilsPanel;
+	}
+	/*
+	private JScrollPane createStepsPanel() 
+	{
+		DefaultListModel<String> DLM = new DefaultListModel<>();
+	    JList<String> stepsList = new JList<>(DLM);
+	    
+	    JScrollPane stepsPanel = new JScrollPane(stepsList);
+	    
+	    if (this.meal != null)
+	    {
+	    	int mealCount = 0;
+	    	for (Recipe recipe : meal.getRecipes())
+	    	{
+	    		mealCount++;
+	    		DLM.addElement(String.format("Meal #%s:\n", mealCount));
+			    int stepCount = 1;
+			    double time = 0;
+			
+			    // Iterate through each step in the recipe and format it
+			    for (Step step : recipe.getSteps()) 
+			    {
+			    	String source = step.getSourceUtensilOrIngredient();
+			    	String destination = " in " + step.getDestinationUtensil();
+			    	String info = source.toString();
+			    	time = step.getTime();
+			    	
+			    	if (source.equals(step.getDestinationUtensil())) 
+			    	{
+			    		destination = "";
+			    	}
+			    	
+			    	for (RecipeIngredient ingredient : recipe.getIngredients())
+			    	{
+			    		if (ingredient.getName().equalsIgnoreCase(step.getSourceUtensilOrIngredient()))
+			    		{
+			    			info = ingredient.toString();
+			    			break;
+			    		}
+			    	}
+			    	String actionText = String.format("Step %d (%s min): %s %s%s%s\n",
+			            stepCount++,
+			            time,
+			            step.getAction(),
+			            info,
+			            destination,
+			            step.getDetails() != null ? " - " + step.getDetails() : ""
+			        );
+			        DLM.addElement(actionText);
+			    }
+	    	}
+	    }
+	    
+	    stepsPanel.setBorder(BorderFactory.createTitledBorder("Steps"));
+	    
+	    // Holds the steps
+	    return stepsPanel;
+	}*/
+	
+	private JScrollPane createStepsPanel() 
+	{
+		DefaultListModel<Recipe> DLM = new DefaultListModel<>();
+	    JList<Recipe> stepsList = new JList<>(DLM);
+	    
+	    JScrollPane stepsPanel = new JScrollPane(stepsList);
+	    
+	    if (this.meal != null)
+	    {
+	    	for (Recipe recipe : meal.getRecipes())
+	    	{
+	    		DLM.addElement(recipe);
+	    	}
+	    }
+	    
+	    stepsList.addMouseListener(new MouseListener()
+        {
+			@Override
+			public void mouseClicked(MouseEvent e) 
+			{
+				Recipe selectedRecipe = stepsList.getSelectedValue();
+				if (selectedRecipe != null) 
+                {
+                    new RecipeProcessViewer(selectedRecipe).setVisible(true);
+                }
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {}
+
+			@Override
+			public void mouseExited(MouseEvent e) {}
+        });
+	    
+	    stepsPanel.setBorder(BorderFactory.createTitledBorder("Recipes"));
+	    
+	    // Holds the steps
+	    return stepsPanel;
+	}
+	
+	private JButton createButton(String imagePath, int width, int height, String toolTipText) 
+	{  
+		ImageIcon icon = createImageIcon(imagePath);
+        JButton button = new JButton();
+
+        if (icon != null) {
+            Image img = icon.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
+            button.setIcon(new ImageIcon(img));
+        } else {
+            button.setText(toolTipText);
+        }
+
+        button.setToolTipText(toolTipText);
+        button.setPreferredSize(new Dimension(width, height));
+        return button;                                                                             
+	}
+	
+	private ImageIcon createImageIcon(String path) {
+        java.net.URL imgURL = getClass().getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Could not find image at " + path);
+            return null;
+        }
+    }
+	
+	public int print(Printable p, JFrame j) throws PrinterException 
+	{
+		PrinterJob job = PrinterJob.getPrinterJob();
+		try
+		{
+			job.setPrintable(p);
+			boolean shouldPrint = job.printDialog();
+			if (shouldPrint)
+				job.print();
+		} 
+		catch (Exception e) 
+		{
+			JOptionPane.showMessageDialog(j, "Unable to print!", "Error", JOptionPane.ERROR_MESSAGE);
+		}
+		return 0;
+	}
+	
+	public void DelegatingPrintable(JComponent delegate)
+    {
+       this.delegate = delegate;
+ 
+       doubleBuffered = delegate.isDoubleBuffered();
+    }
+	
+	@Override
+    public int print(Graphics graphics, PageFormat pageFormat, int pageIndex) throws PrinterException {
+        if (pageIndex > 0) {
+            return NO_SUCH_PAGE;
+        }
+
+        Graphics g2d = graphics;
+        g2d.translate((int) pageFormat.getImageableX(), (int) pageFormat.getImageableY());
+        // double scaleX = pageFormat.getImageableWidth() / delegate.getWidth();
+        //double scaleY = pageFormat.getImageableHeight() / delegate.getHeight();
+        // double scale = Math.min(scaleX, scaleY);
+        // g2d.scale(scale, scale);
+
+        delegate.setDoubleBuffered(false);
+        delegate.paint(g2d);
+        delegate.setDoubleBuffered(doubleBuffered);
+
+        return PAGE_EXISTS;
+    }
+	
+	public static void main(String[] args) 
+	{
+		List<Recipe> recipes = Recipe.getRecipes();
+	    SwingUtilities.invokeLater(() -> {
+	        Meal mealTest = new Meal("Test meal", recipes);
+	        new MealProcessViewer().setVisible(true);
+	    });
 	}
 
 }
--- src/gui/RecipeProcessViewer.java
+++ src/gui/RecipeProcessViewer.java
@@ -1,22 +1,20 @@
 package gui;
 
-import java.awt.BorderLayout;
-import java.awt.Graphics;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.print.*;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.swing.BorderFactory;
-import javax.swing.DefaultListModel;
-import javax.swing.JFrame;
-import javax.swing.JList;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
+
 import Information.Recipe;
 import Information.RecipeIngredient;
 import Information.Step;
 import Information.Utensil;
-import java.awt.print.*;
-import java.util.List;
+
+
+import gui.EditorListeners.*;
 
 /**
  * Creates the window to display utensils and steps in a recipe or meal.
@@ -27,91 +25,128 @@ public class RecipeProcessViewer extends JFrame implements Printable
 {
 
 	private static final long serialVersionUID = 1L;
+	private static final String PRINT_ICON_PATH = "/img/print.png";
+	private static final String OPEN_ICON_PATH = "/img/open.png";
 	private Recipe recipe;
+	private boolean doubleBuffered;
+    private JComponent delegate;
+    private JButton printButton, openButton;
+    private JPanel printPanel, mainPanel, utensilsPanel, stepsPanel;
 	
 	public RecipeProcessViewer() 
 	{
-	    setTitle("Recipe Viewer");
-	    setSize(500, 500);
-	    setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-	    setLayout(new BorderLayout());
-	    
-	    // Main panel
-	    JPanel mainPanel = new JPanel(new BorderLayout());
-	    // JPanel printButton = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 10));
-	    
-	    // Add print button
-	    // buttonCreation button = new buttonCreation();
-	    // button.addImageButton(printButton, "/img/print.png", "Print");
-	    
-	    // mainPanel.add(printButton, BorderLayout.NORTH);
-	
-	    // Utensils panel
-	    JScrollPane utensilsPanel = createEmptyPanel("Utensils");
-	    utensilsPanel.setSize(200, 200);
-	    mainPanel.add(utensilsPanel, BorderLayout.CENTER);
-	
-	    // Steps panel
-	    JScrollPane stepsPanel = createEmptyPanel("Steps");
-	    stepsPanel.setSize(200, 200);
-	    mainPanel.add(stepsPanel, BorderLayout.SOUTH);
-	
-	    add(mainPanel);
+	    initialize();
 	}
 	
 	public RecipeProcessViewer(Recipe recipe) 
 	{
-	    this.recipe = recipe;
-	
-	    setTitle("Recipe Viewer   " + recipe.getName());
-	    setSize(500, 500);
-	    setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-	    setLayout(new BorderLayout());
-	    
-	    // Main panel
-	    JPanel mainPanel = new JPanel(new BorderLayout());
-	    // JPanel printButton = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 10));
-	    
-	    // Add print button
-	    // buttonCreation button = new buttonCreation();
-	    // button.addImageButton(printButton, "/img/print.png", "Print");
-	
-	    // Utensils panel
-	    JScrollPane utensilsPanel = createUtensilsPanel();
-	    utensilsPanel.setSize(100, 100);
-	    mainPanel.add(utensilsPanel, BorderLayout.CENTER);
-	
-	    // Steps panel
-	    JScrollPane stepsPanel = createStepsPanel();
-	    stepsPanel.setSize(200, 200);
-	    mainPanel.add(stepsPanel, BorderLayout.SOUTH);
-	
-	    add(mainPanel);
+		this.recipe = recipe;
+        initialize();
+        setTitle("KiLowBites Process Viewer: " + recipe.getName());
 	}
 	
-	private JScrollPane createEmptyPanel(String title) {
-        JScrollPane emptyPanel = new JScrollPane();
-        emptyPanel.setBorder(BorderFactory.createTitledBorder(title));
-        return emptyPanel;
+	private void initialize() 
+	{
+		setTitle("KiLowBites Process Viewer");
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        mainPanel = new JPanel(new BorderLayout());
+        printPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 10));
+        mainPanel.add(printPanel, BorderLayout.NORTH);
+        
+        // Add utensils 
+        utensilsPanel = new JPanel(new BorderLayout());
+	    JScrollPane utensilsScroll = createUtensilsPanel();
+	    utensilsPanel.add(utensilsScroll, BorderLayout.CENTER);
+	    utensilsPanel.setPreferredSize(new Dimension(200, 100));
+
+        // Add steps
+        stepsPanel = new JPanel(new BorderLayout());
+        JScrollPane stepsScroll = createStepsPanel();
+        stepsPanel.add(stepsScroll, BorderLayout.CENTER);
+	    stepsPanel.setPreferredSize(new Dimension(200, 300));
+	    
+	    // Add both to main panel
+	    mainPanel.add(utensilsPanel, BorderLayout.CENTER);
+        mainPanel.add(stepsPanel, BorderLayout.SOUTH);
+
+        add(mainPanel, BorderLayout.CENTER);
+        addPrintButton();
+        addOpenButton();
+    }
+	
+	private void addPrintButton() 
+	{
+        printButton = createButton(PRINT_ICON_PATH, 50, 50, "Print");
+        printButton.addActionListener(new ActionListener() 
+        {
+            @Override
+            public void actionPerformed(ActionEvent e) 
+            {
+                printRecipe();
+            }
+        });
+        printPanel.add(printButton);
+    }
+	
+	private void addOpenButton()
+	{
+		openButton = createButton(OPEN_ICON_PATH, 50, 50, "Open");
+        openButton.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e) 
+            {
+                new RecipeSearcher().setVisible(true);
+            }
+        });
+        printPanel.add(openButton);
+	}
+	
+	private void printRecipe() 
+	{
+        PrinterJob job = PrinterJob.getPrinterJob();
+        job.setPrintable(this);
+
+        if (job.printDialog()) 
+        {
+            try 
+            {
+                job.print();
+            } 
+            catch (PrinterException e) 
+            {
+                JOptionPane.showMessageDialog(this, "Unable to print", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
     }
 	
 	private JScrollPane createUtensilsPanel() 
 	{
 		
 		JScrollPane utensilsPanel = new JScrollPane();
+        utensilsPanel.setPreferredSize(new Dimension(10,1000));
+		JList<String> utensilsList;
+		DefaultListModel<String> DLM;
 		
-		DefaultListModel<String> DLM = new DefaultListModel<>();
-	    JList<String> utensilsList = new JList<>(DLM);
-	
-	    // Add items to utensils list
-	    for (Utensil utensil : recipe.getUtenils())	    
-	    {
-            String name = utensil.getName();
-            String details = utensil.getDetails() != null ? utensil.getDetails() : "No description";
-            DLM.addElement(name + ": " + details);
-        }
-	    
-	    utensilsPanel = new JScrollPane(utensilsList);
+		if (this.recipe != null) 
+		{
+			DLM = new DefaultListModel<>();
+		    utensilsList = new JList<>(DLM);
+		
+		    // Add items to utensils list
+		    for (Utensil utensil : recipe.getUtenils())	    
+		    {
+		        String name = utensil.getName();
+		        String details = utensil.getDetails() != null ? utensil.getDetails() : "No description";
+		        DLM.addElement(name + ": " + details);
+		    }
+		    
+		    utensilsPanel = new JScrollPane(utensilsList);
+		}
 	    
 	    // Create border with title
 	    utensilsPanel.setBorder(BorderFactory.createTitledBorder("Utensils"));
@@ -125,56 +160,81 @@ public class RecipeProcessViewer extends JFrame implements Printable
 	    JList<String> stepsList = new JList<>(DLM);
 	    
 	    JScrollPane stepsPanel = new JScrollPane(stepsList);
-	    stepsPanel = new JScrollPane(stepsList);
-	    stepsPanel.setBorder(BorderFactory.createTitledBorder("Steps"));
 	    
-	    int stepCount = 1;
-
-	    // Iterate through each step in the recipe and format it
-	    for (Step step : recipe.getSteps()) 
+	    if (this.recipe != null)
 	    {
-	    	String source = step.getSourceUtensilOrIngredient();
-	    	String destination = "in " + step.getDestinationUtensil();
-	    	Double amount;
-	    	String unit;
-	    	
-	    	if (source.equals(step.getDestinationUtensil())) 
-	    	{
-	    		destination = "";
-	    	}
-	    	
-	    	int ingredientCount = 0;
-	    	
-	    	if (source.equalsIgnoreCase(recipe.getIngredients().get(ingredientCount).getName())) 
-	    	{
-	    		amount = recipe.getIngredients().get(0).getAmount();
-	            unit = recipe.getIngredients().get(0).getUnit();
-	    	} 
-	    	else
-	    	{
-	    		amount = recipe.getIngredients().get(1).getAmount();
-	            unit = recipe.getIngredients().get(1).getUnit();
-	    	}
-	    	
-	    	String actionText = String.format("Step %d: %s %s %s %s %s%s\n",
-	            stepCount++,
-	            step.getAction(),
-	            amount,
-	            unit,
-	    	    source,
-	            destination,
-	            step.getDetails() != null ? " - " + step.getDetails() : ""
-	        );
-	        DLM.addElement(actionText);
+		    int stepCount = 1;
+		    double time = 0;
+		
+		    // Iterate through each step in the recipe and format it
+		    for (Step step : recipe.getSteps()) 
+		    {
+		    	String source = step.getSourceUtensilOrIngredient();
+		    	String destination = " in " + step.getDestinationUtensil();
+		    	String info = source.toString();
+		    	time = step.getTime();
+		    	
+		    	if (source.equals(step.getDestinationUtensil())) 
+		    	{
+		    		destination = "";
+		    	}
+		    	
+		    	for (RecipeIngredient ingredient : recipe.getIngredients())
+		    	{
+		    		if (ingredient.getName().equalsIgnoreCase(step.getSourceUtensilOrIngredient()))
+		    		{
+		    			info = ingredient.toString();
+		    			break;
+		    		}
+		    	}
+		    	
+		    	String actionText = String.format("Step %d (%s min): %s %s%s%s\n",
+		            stepCount++,
+		            time,
+		            step.getAction(),
+		            info,
+		            destination,
+		            step.getDetails() != null ? " - " + step.getDetails() : ""
+		        );
+		        DLM.addElement(actionText);
+		    }
 	    }
-
-	    // Set the formatted steps in the JTextArea
+	    
+	    stepsPanel.setBorder(BorderFactory.createTitledBorder("Steps"));
 	    
 	    // Holds the steps
 	    return stepsPanel;
 	}
 	
-	public int print(Printable p, JFrame j) throws PrinterException {
+	private JButton createButton(String imagePath, int width, int height, String toolTipText) 
+	{  
+		ImageIcon icon = createImageIcon(imagePath);
+        JButton button = new JButton();
+
+        if (icon != null) {
+            Image img = icon.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
+            button.setIcon(new ImageIcon(img));
+        } else {
+            button.setText(toolTipText);
+        }
+
+        button.setToolTipText(toolTipText);
+        button.setPreferredSize(new Dimension(width, height));
+        return button;                                                                             
+	}
+	
+	private ImageIcon createImageIcon(String path) {
+        java.net.URL imgURL = getClass().getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Could not find image at " + path);
+            return null;
+        }
+    }
+	
+	public int print(Printable p, JFrame j) throws PrinterException 
+	{
 		PrinterJob job = PrinterJob.getPrinterJob();
 		try
 		{
@@ -182,22 +242,45 @@ public class RecipeProcessViewer extends JFrame implements Printable
 			boolean shouldPrint = job.printDialog();
 			if (shouldPrint)
 				job.print();
-		} catch (Exception e) {
+		} 
+		catch (Exception e) 
+		{
 			JOptionPane.showMessageDialog(j, "Unable to print!", "Error", JOptionPane.ERROR_MESSAGE);
 		}
 		return 0;
 	}
 	
+	public void DelegatingPrintable(JComponent delegate)
+    {
+       this.delegate = delegate;
+ 
+       doubleBuffered = delegate.isDoubleBuffered();
+    }
+	
 	@Override
-	public int print(Graphics graphics, PageFormat pageFormat, int pageIndex) throws PrinterException {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    public int print(Graphics graphics, PageFormat pageFormat, int pageIndex) throws PrinterException {
+        if (pageIndex > 0) {
+            return NO_SUCH_PAGE;
+        }
+
+        Graphics g2d = graphics;
+        g2d.translate((int) pageFormat.getImageableX(), (int) pageFormat.getImageableY());
+        // double scaleX = pageFormat.getImageableWidth() / delegate.getWidth();
+        //double scaleY = pageFormat.getImageableHeight() / delegate.getHeight();
+        // double scale = Math.min(scaleX, scaleY);
+        // g2d.scale(scale, scale);
+
+        delegate.setDoubleBuffered(false);
+        delegate.paint(g2d);
+        delegate.setDoubleBuffered(doubleBuffered);
+
+        return PAGE_EXISTS;
+    }
 	
 	public static void main(String[] args) 
 	{
 	    SwingUtilities.invokeLater(() -> {
-	        Recipe bananasFoster = Recipe.getRecipes().get(1);
+	        Recipe bananasFoster = Recipe.getRecipes().get(0);
 	        new RecipeProcessViewer(bananasFoster).setVisible(true);
 	    });
 	}
